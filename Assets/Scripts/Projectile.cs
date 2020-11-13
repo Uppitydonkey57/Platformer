780e4090968150375dad6e440a2509d962293711
Zach <zachgeyer07@gmail.com>
2020-11-12 20:16:40 -0500

    I removed some unused methods.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,17 +6,6 @@ public class Projectile : MonoBehaviour
 {
 
     public float damage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
