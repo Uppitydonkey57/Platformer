ffd06d27384d0450212a863841b4b17b2fcb9304
Zach <zachgeyer07@gmail.com>
2020-11-29 17:47:21 -0500

    Added first particles and drew wizard.

---
+++ Assets/Scripts/PlayParticle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticle : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    public void ParticlePlay()
+    {
+        if (particle != null)
+            particle.Play();
+    }
+}
