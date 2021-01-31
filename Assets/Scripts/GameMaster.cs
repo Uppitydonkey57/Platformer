756ac159a0179a2994b2b97bb1d67954c9cab5cc
zachgeyer07@gmail.com <zachgeyer07@gmail.com>
2021-01-31 18:55:06 -0500

    Added Dotween

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using DG.Tweening;
+using Cinemachine;
 
 public class GameMaster : MonoBehaviour
 {
@@ -10,13 +12,23 @@ public class GameMaster : MonoBehaviour
 
     public bool isMatsMode;
 
+    public Transform reloadCircle;
+
+    PlayerController player;
+
     Controls controls;
+
+    CinemachineBrain brain;
 
     private void Start()
     {
         controls = new Controls();
 
         controls.Enable();
+
+        brain = Camera.main.GetComponent<CinemachineBrain>();
+
+        player = FindObjectOfType<PlayerController>();
 
         if (isMatsMode)
         {
@@ -32,6 +44,16 @@ public class GameMaster : MonoBehaviour
     private void OnDisable()
     {
         controls.Disable();
+    }
+
+    public IEnumerator PlayerDeathReset()
+    {
+        yield return null;
+    }
+
+    public IEnumerator LoadLevel()
+    {
+        yield return null;
     }
 
     // Update is called once per frame
